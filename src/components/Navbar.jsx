5c0a7f7f2b5624cb9b8e7407fe0cfef1a0577d44
pedro0Ag <mesquitapedro752@gmail.com>
2024-07-16 19:22:27 -0300

    imagem arrumando

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { FaShoppingCart } from 'react-icons/fa';
 const Navbar = () => {
   return (
     <nav className="bg-yellow-300 p-4 flex justify-between">
-      <Link to="/" className="text-xl font-bold"><img className="w-[60px] h-[60px]"src="/GOAT/public//ca.png" alt="" /></Link>
+      <Link to="/" className="text-xl font-bold"><img className="w-[60px] h-[60px]"src="GOAT/public/ca.png" alt="" /></Link>
       <div className="flex space-x-4">
         <Link to="/" className="hover:underline">Home</Link>
         <Link to="/products" className="hover:underline">Products</Link>
