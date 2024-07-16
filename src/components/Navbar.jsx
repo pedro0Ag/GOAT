f72fba846363e80bf029eeb87d1af8d14e56ed94
pedro0Ag <mesquitapedro752@gmail.com>
2024-07-16 19:08:40 -0300

    imagem mais cor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import { FaShoppingCart } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
-    <nav className="bg-blue-600 p-4 text-white flex justify-between">
-      <Link to="/" className="text-xl font-bold"><img className="w-[60px] h-[60px]"src="/public/ca.png" alt="" /></Link>
+    <nav className="bg-yellow-300 p-4 flex justify-between">
+      <Link to="/" className="text-xl font-bold"><img className="w-[60px] h-[60px]"src="/GOAT/public//ca.png" alt="" /></Link>
       <div className="flex space-x-4">
         <Link to="/" className="hover:underline">Home</Link>
         <Link to="/products" className="hover:underline">Products</Link>
